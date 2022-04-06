cb65f166958eaa90f8bc0ace79ca46358510e6a9
sonhl0723 <sonhl0723@naver.com>
2022-04-06 17:18:12 +0900

    web 진행중

--- IVCS_Server/main.js
+++ IVCS_Server/main.js
@@ -13,7 +13,9 @@ app.get("/client", (req, res) => {
 
 // socket
 io.on('connection',function(socket){
-    console.log("New Client!");
+    const req = socket.request;
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log('새로운 클라이언트 접속', ip, socket.id, req.ip);
 
     socket.on("start", (data) => {
         console.log(data)
@@ -24,13 +26,13 @@ io.on('connection',function(socket){
     socket.on("changeCCTV", (data) => {
         console.log(data)
     })
-    socket.on("test", (tmp)=>{
-        console.log(tmp.toString());
+    socket.on("test", ()=>{
+        console.log("test");
 
         // 1. child-process모듈의 spawn 취득
         const spawn = require('child_process').spawn;
         // 2. spawn을 통해 "python 파이썬파일.py" 명령어 실행
-        const result = spawn('python3', ['./pytorch/test.py', 'testParameter']);
+        const result = spawn('python3', ['./pytorch/test.py', '1', "[수도권제1순환선] 성남"]);
         // 3. stdout의 'data'이벤트리스너로 실행결과를 받는다.
         result.stdout.on('data', function(data) { console.log(data.toString()); });
         // 4. 에러 발생 시, stderr의 'data'이벤트리스너로 실행결과를 받는다.
