55d56ced66529b047e6928990edc29839fa2a17b
1004gite <1004gite@naver.com>
2022-05-12 18:08:02 +0900

    불필요한 파일 제거

--- IOS/IVCS_IOS/Pods/Target Support Files/Pods-IVCS_IOS-IVCS_IOSUITests/Pods-IVCS_IOS-IVCS_IOSUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IVCS_IOS_IVCS_IOSUITests : NSObject
-@end
-@implementation PodsDummy_Pods_IVCS_IOS_IVCS_IOSUITests
-@end
--- IOS/IVCS_IOS/Pods/Target Support Files/Pods-IVCS_IOSTests/Pods-IVCS_IOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IVCS_IOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_IVCS_IOSTests
-@end
