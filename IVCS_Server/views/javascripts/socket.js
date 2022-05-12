5ce8881b211c47e51a1b1a326f8940f0b2eba89e
sonhl0723 <sonhl0723@naver.com>
2022-05-12 17:01:22 +0900

    connect-plus-1.0.0 템플렛 적용

--- IVCS_Server/views/javascripts/socket.js
+++ IVCS_Server/views/javascripts/socket.js
@@ -3,31 +3,70 @@ const socket_data = io('http://localhost:4000');
 
 let xhr = new XMLHttpRequest();
 
-const selected_camera = document.getElementById('camera');
-const count_text = document.getElementsByClassName('counting_text')[0]
+const play_camera = document.getElementsByName('play_button')[0];
+const selected_camera = document.getElementsByName('count-camera-id')[0];
+
+const count_text = document.getElementsByClassName('counting_text')[0];
 
 var analysis_camera = document.getElementById('analysis_camera_id');
-var measure_unit = document.getElementById('measure_unit')
-var start_date = document.getElementById('start_date')
-var end_date = document.getElementById('end_date')
-var start_time = document.getElementById('start_time')
-var end_time = document.getElementById('end_time')
+var measure_unit = document.getElementById('measure_unit');
+var start_date = document.getElementById('start_date');
+var end_date = document.getElementById('end_date');
+var start_time = document.getElementById('start_time');
+var end_time = document.getElementById('end_time');
 
-var plot_img = document.getElementById('analysis_result')
+var camera_list = document.getElementsByTagName('ul')[1];
 
-var counting_camera = [];
+var camera_json;
+var plot_img = document.getElementById('analysis_result');
 
-selected_camera.addEventListener('submit', (e) => {
-    e.preventDefault();
-    var camera_id = e.target.camera_id.value
-    counting_camera.push(e.target.camera_id.value);
-    socket.emit('hls_req', camera_id);
-    e.target.camera_id.value = '';
-})
+var hls;
+
+var req_counting_interval;
+
+var hls_flag = false;
+var img_flag = false;
+var count_flag = false;
+
+$.getJSON('http://localhost:3000/getUrl', function(data) {
+    camera_json = JSON.parse(data.replace(/'/g, '"'))
+    for(var i=0; i<camera_json['cctvname'].length; i++){
+        const li = document.createElement("li");
+        li.className = "nav-item";
+        li.setAttribute('value', i);
+        if(i==0) li.innerHTML = '<a class="nav-link active" id='+i+' data-toggle="tab" href="#business-1" role="tab">'+camera_json['cctvname'][i]+'</a>';
+        else li.innerHTML = '<a class="nav-link" id='+i+' data-toggle="tab" href="#business-1" role="tab">'+camera_json['cctvname'][i]+'</a>';
+        camera_list.appendChild(li);
+        li.addEventListener('click', function(){
+            if(count_flag){
+                count_flag = false;
+                clearInterval(req_counting_interval);
+                count_text.innerHTML = "---";
+            }
+
+            if(hls_flag){
+                hls.detachMedia();;
+                hls_flag = false;
+            }
+
+            if(img_flag){
+                plot_img.src = "";
+                img_flag = false;
+            }
+        });
+        if(i==1) break; // 데모를 위해 2개만 보여줌
+    }
+});
+
+function get_camera_id(){
+    socket.emit('hls_req', document.getElementsByClassName("active")[1].id);
+}
 
 function req_counting_flag() {
-    setInterval(function () { 
-        socket_data.emit('req_counting', counting_camera[counting_camera.length-1]); 
+    count_flag = true;
+    req_counting_interval = setInterval(function () { 
+        socket_data.emit('req_counting', document.getElementsByClassName("active")[1].id);
+        console.log("plz");
     }, 2000);
 }
 
@@ -38,7 +77,7 @@ let sendData = () => {
 
     let data = {
         "measure_method": measure_unit.value,
-        "cameraid": analysis_camera.value,
+        "cameraid": document.getElementsByClassName("active")[1].id,
         "start" : start_date.value+"_"+start_time.value,
         "end" : end_date.value+"_"+end_time.value,
     };
@@ -54,6 +93,7 @@ function analysis(){
         var arrayBuffer = xhr.response;
         var byteArray = new Uint8Array(arrayBuffer);
         plot_img.src =  URL.createObjectURL( new Blob([byteArray.buffer], { type: 'image/png' } ));
+        img_flag = true;
     }
 }
 
@@ -85,12 +125,13 @@ socket.on('hls_res', (hls_url) => {
         // If no native HLS support, check if HLS.js is supported
         //
     } else if (Hls.isSupported()) {
-        var hls = new Hls();
+        hls = new Hls();
         hls.loadSource(videoSrc);
         hls.attachMedia(video);
+        hls_flag = true;
     }
 })
 
 socket_data.on('res_counting', (count) => {
-    count_text.innerHTML = count;
+    count_text.innerHTML = String(count);
 })
