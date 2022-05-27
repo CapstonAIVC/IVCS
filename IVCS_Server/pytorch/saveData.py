16dfc2ea6f5610122f0d00e26babb91776116565
1004gite <1004gite@naver.com>
2022-05-27 14:42:33 +0900

    hotfix

--- IVCS_Server/pytorch/saveData.py
+++ IVCS_Server/pytorch/saveData.py
@@ -112,7 +112,7 @@ def startCounting(sid, cctvIdx):
 def startCounting(sid, cctvIdx):
     global count, input_img, density
     # sio.emit('res_counting', str(round(latest[int(cctvIdx)[0]], 3)), sid)
-    sio.emit('res_counting', data=(str(round(count[int(cctvIdx)])), str(input_img[int(cctvIdx)]), str(density[int(cctvIdx)]), room=sid)
+    sio.emit('res_counting', data=(str(round(count[int(cctvIdx)])), str(input_img[int(cctvIdx)]), str(density[int(cctvIdx)])), room=sid)
     
 @app.route('/req_plot', methods=['POST'])
 def res_plot_png():
