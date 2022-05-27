c6f692a2bc1c85a98c5356d82f27155f3d087c3b
sonhl0723 <sonhl0723@naver.com>
2022-05-27 15:30:25 +0900

    hls 주소 클라이언트에서 파싱

--- IVCS_Server/main.js
+++ IVCS_Server/main.js
@@ -26,8 +26,8 @@ app.get("/main", (req, res) => {
 app.get("/getUrl", (req, res) => {
     res.json(information);
 })
-app.get("/getUrl_mobile", (req, res) => {
-    res.send(information);
+app.get("/getUrl_web", (req, res) => {
+    res.json(information_client);
 })
 app.get("/getUrl_client", (req, res) => {
     res.send(information_client);
@@ -81,18 +81,16 @@ server.listen(3000,()=>{
         getUrl_result.stderr.on('data', function(data) { console.log(data.toString()); });
         
         information = data.toString().split("\n")[0]
-        // information_json = JSON.parse(information.replace(/'/g, '"'))
+        information_json = JSON.parse(information.replace(/'/g, '"'))
 	    // console.log(information_json)
         console.log(' The info is ready!!\n');
 
         const getUrl_client_spawn = require('child_process').spawn;
         const getUrl_client_result = getUrl_client_spawn('python3', ['./pytorch/getLinkForClient.py', '1', api_key]);
         getUrl_client_result.stdout.on('data', (data) => {
-            getUrl_result.stderr.on('data', function(data) { console.log(data.toString()); });
+            getUrl_client_result.stderr.on('data', function(data) { console.log(data.toString()); });
             
             information_client = data.toString().split("\n")[0]
-            information_json = JSON.parse(information.replace(/'/g, '"'))
-            // console.log(information_json)
             console.log(' The Client info is ready!!\n');
         });
     });
